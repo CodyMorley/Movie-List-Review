752d1206bece09c8d82b3372fd856aba373da317
Cody Morley <codymorleyinc@gmail.com>
2020-08-24 14:18:26 -0700

    Model
    Movie model finished x2

---
+++ Movie List Review - Notification Pattern/Movie List/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Cody Morley on 8/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var title: String
+    var seen: Bool = false
+}
